b18aab6e55e4ac7ed11dcf402995e735316fc6ff
Marinoyyy <111189140+Marinoyyy@users.noreply.github.com>
2025-09-19 10:06:09 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -o errexit
 pip install -r requirements.txt
 
 
-flask db migrate -m "Adiciona coluna observacoes em Avaliacao"
+#flask db migrate -m "Adiciona coluna observacoes em Avaliacao"
 
 flask db upgrade
 
